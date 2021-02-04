90013808134568fdd1a44f23d996106de376880a
Paul Franco <mail@paulfran.co>
2021-02-04 05:08:05 -0800

    Created a Fake Repository for Testing

--- app/src/test/java/co/paulfran/tddshoppinglistapp/repositories/FakeShoppingRepository.kt
+++ app/src/test/java/co/paulfran/tddshoppinglistapp/repositories/FakeShoppingRepository.kt
@@ -1,4 +1,55 @@
 package co.paulfran.tddshoppinglistapp.repositories
 
-class FakeShoppingRepository {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import co.paulfran.tddshoppinglistapp.data.local.ShoppingItem
+import co.paulfran.tddshoppinglistapp.data.remote.responses.ImageResponse
+import co.paulfran.tddshoppinglistapp.other.Resource
+
+class FakeShoppingRepository: ShoppingRepository {
+
+    private val shoppingItems = mutableListOf<ShoppingItem>()
+    private val observableShoppingItems = MutableLiveData<List<ShoppingItem>>(shoppingItems)
+    private val observableTotalPrice = MutableLiveData<Float>()
+
+    private var shouldReturnNetworkError = false
+
+    fun setShouldReturnNetworkError(value: Boolean) {
+        shouldReturnNetworkError = value
+    }
+
+    override suspend fun insertShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.add(shoppingItem)
+        refreshLiveData()
+    }
+
+    override suspend fun deleteShoppingItem(shoppingItem: ShoppingItem) {
+        shoppingItems.remove(shoppingItem)
+        refreshLiveData()
+    }
+
+    override fun observeAllShoppingItems(): LiveData<List<ShoppingItem>> {
+        return observableShoppingItems
+    }
+
+    override fun observeTotalPrice(): LiveData<Float> {
+        return observableTotalPrice
+    }
+
+    override suspend fun searchForImage(imageQuery: String): Resource<ImageResponse> {
+        return if (shouldReturnNetworkError) {
+            Resource.error("Error", null)
+        } else {
+            Resource.success(ImageResponse(listOf(), 0, 0))
+        }
+    }
+
+    private fun refreshLiveData() {
+        observableShoppingItems.postValue(shoppingItems)
+        observableTotalPrice.postValue(getTotalPrice())
+    }
+
+    private fun getTotalPrice(): Float {
+        return shoppingItems.sumByDouble { it.price.toDouble() }.toFloat()
+    }
 }
